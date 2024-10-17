cf0a93c56e1a87ae4c56f5817a2459939c603acc
ahhhh W <noahwhelden@gmail.com>
2024-10-17 18:27:54 -0400

    Create memberRoutes.js
    routing for membersController CRUD methods

---
+++ routes/memberRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const membersController = require('../controllers/membersController');
+
+// Routing for Members data HTTP methods
+router.route('/')
+    .get(membersController.getAllMembers) // read
+    .post(membersController.createNewMember) // create
+    .patch(membersController.updateMember) // update
+    .delete(membersController.deleteMember); // delete
+
+module.exports = router;

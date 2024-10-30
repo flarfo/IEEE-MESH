8f631af467a93585fe83c3af2773f2d76da21fd2
ahhhh W <noahwhelden@gmail.com>
2024-10-30 16:22:40 -0400

    Add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,11 @@
-import React, {useState} from 'react';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
-import MainLayout from './layouts/MainLayout';
-import Login from './pages/Login';
-import SignUp from './pages/Register';
-
-function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route 
-        path="/login"
-        element={<Login />}
-      />
-      <Route 
-        path='/sign-up' 
-        element={<Register />}
-      />
-    </Route>
-    )
-  )
-  return <RouterProvider router={router} />
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
 export default App;
---
+++ client/src/constants/index.js
@@ -0,0 +1,4 @@
+
+export const navItems = [
+    { label: 'Database', href: '#' },
+];

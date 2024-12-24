a62d4e666ba09dd346ea98bf89a9111ee43506a3
ahhhh W <noahwhelden@gmail.com>
2024-12-23 20:43:56 -0500

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 // Define allowed origins here, these are the origins that can make API requests to the server
 const allowedOrigins = [
+    'http://localhost:3000', // local client
     'http://localhost:3500', // local server
 ];
 

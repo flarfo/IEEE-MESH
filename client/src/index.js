1e5f4bd7d0ed7979654d03be26e73357486bb5da
ahhhh W <noahwhelden@gmail.com>
2025-02-11 11:01:12 -0500

    Add Ant Design to CSS

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-
+import 'antd/dist/reset.css';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 

6ab8beef4434fe5e254424d3b45756f59a44fab3
ahhhh W <noahwhelden@gmail.com>
2025-02-11 11:01:41 -0500

    Update membersController.js

--- server/controllers/membersController.js
+++ server/controllers/membersController.js
@@ -1,6 +1,5 @@
 const Member = require('../models/Member');
-
-// TODO: add user linking and member creation
+const User = require('../models/User');
 
 // HTTP controller methods for Members data (Routing found in routes/memberRoutes)
 
@@ -21,74 +20,44 @@ const getAllMembers = async (req, res) => {
     res.json(members);
 };
 
-// @desc Create new member
-// @route POST /members
-// @access Private
-const createNewMember = async (req, res) => {
-    const { name, email, internships, research } = req.body;
-
-    // Confirm that data is valid
-    if (!name || !email) {
-        // if missing required information, return JSON with bad request message
-        return res.status(400).json({ message: 'Name and email fields are required.' });
-    }
-
-    // Check for duplicate information
-    const duplicate = await Member.findOne({ email }).lean().exec();
-
-    if (duplicate) {
-        // if duplicate, return JSON with conflict message
-        return res.status(409).json({ message: 'Duplicate email.' });
-    }
-
-    const memberObject = { name, email, internships, research };
-
-    // Create and store the new member
-    const member = await Member.create(memberObject);
-
-    if (member) {
-        res.status(201).json({ message: `New member ${name} created.` });
-    }
-    else {
-        res.status(400).json({ message: 'Invalid member data received.' });
-    }
-};
-
 // @desc Update a member
 // @route PATCH /members
 // @access Private
 const updateMember = async (req, res) => {
-    const {id, name, email, roles, internships, research } = req.body;
-    // TODO: id not obtained by request
+    // TODO change member setting
+    const {id, username, name, email, internships, research } = req.body;
     // Confirm that data is valid
-    console.log(id);
-    if (!id || !name || !email) {
-        return res.status(400).json({ message: 'Name and email fields are required.' });
+    const user = await User.findById(id);
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found.' });
     }
 
-    const member = Member.findById(id).exec();
+    // Ensure requesting user is authorized to update
+    if (user.username === req.username || req.roles.includes('Admin')) {
 
-    if (!member) {
-        return res.status(400).json({ message: 'Member not found.' });
+        const member = Member.findById(user.member);
+        // If no member exists in the user profile, create a new one and update
+        if (!member) {
+            const memberObject = { name, email, internships, research };
+            const newMember = await Member.create(memberObject);
+            user.member = newMember._id;
+            await user.save();
+
+            return res.json({ message: `${newMember.name} updated.` });
+        }
+
+        member.name = name;
+        member.email = email;
+        member.internships = internships;
+        member.research = research;
+
+        const updatedMember = await member.save();
+
+        return res.json({ message: `${updatedMember.name} updated.` });
     }
-
-    const duplicate = await Member.findOne({ email }).lean().exec();
-
-    // Verify that received id equals original id (the existing member we are searching for, not an actual duplicate) 
-    if (duplicate && duplicate?._id.toString() !== id) {
-        // if duplicate, return JSON with conflict message
-        return res.status(409).json({ message: 'Duplicate email.' });
-    }
-
-    member.name = name;
-    member.email = email;
-    member.roles = roles;
-    member.internships = internships;
-    member.research = research;
-
-    const updatedMember = await member.save();
-
-    res.json({ message: `${updatedMember.name} updated.` });
+    
+    return res.status(400).json({ message: 'Unauthorized.' });
 };
 
 // @desc Delete a member
@@ -96,26 +65,33 @@ const updateMember = async (req, res) => {
 // @access Private
 const deleteMember = async (req, res) => {
     const { id } = req.body;
+    // Confirm that data is valid
+    const user = await User.findById(id);
 
-    if (!id) {
-        return res.status(400).json({ message: 'Member ID required.' })
+    if (!user) {
+        return res.status(400).json({ message: 'User not found.' });
     }
 
-    const member = await Member.findById(id).exec();
+    // Ensure requesting user is authorized to update
+    if (user.username === req.username || req.roles.includes('Admin')) {
 
-    if (!member) {
-        return res.status(400).json({ message: 'Member not found.' });
+        const member = Member.findById(user.member);
+        // If no member exists in the user profile, create a new one and update
+        if (!member) {
+            return res.status(400).json({ message: 'Member not found.' });
+        }
+
+        // Delete member
+        const result = await member.deleteOne();
+        const reply = `Name ${result.name} with ID ${result._id} deleted.`
+        res.json(reply);
     }
-
-    const result = await member.deleteOne();
-
-    const reply = `Name ${result.name} with ID ${result._id} deleted.`
-    res.json(reply);
+    
+    return res.status(400).json({ message: 'Unauthorized.' });
 };
 
 module.exports = {
     getAllMembers,
-    createNewMember,
     updateMember,
     deleteMember
 };

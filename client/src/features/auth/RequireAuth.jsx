7f758c26d2a9f4c72f628b5bb2f6bcfe3a3c0b69
ahhhh W <noahwhelden@gmail.com>
2025-02-11 11:00:16 -0500

    Add "checkSpecificUser"
    Checks if the logged in user attempting to enter the protected page (found at ../example/user) matches the user information located at the protected URL

--- client/src/features/auth/RequireAuth.jsx
+++ client/src/features/auth/RequireAuth.jsx
@@ -1,13 +1,17 @@
-import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { useLocation, useParams, Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
-const RequireAuth = ({ allowedRoles }) => {
+const RequireAuth = ({ allowedRoles, checkSpecificUser }) => {
     const location = useLocation();
-    const { roles } = useAuth();
+    const { roles, username } = useAuth();
+    const params = useParams();
 
+    const verifiedUser = (checkSpecificUser ? username === params.username : true);
+    const verifiedRoles = roles.some(role => allowedRoles.includes(role));
+    
     const content = (
         // Check if some role in current users roles is allowed, if yes provide an outlet, if no, reroute to login
-       (roles.some(role => allowedRoles.includes(role))) ? <Outlet /> : <Navigate to='/login' state={{ from: location}} replace />
+       (verifiedUser && verifiedRoles) ? <Outlet /> : <Navigate to='/login' state={{ from: location}} replace />
     );
 
     return content;
